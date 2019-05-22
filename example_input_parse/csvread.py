07fdb77eaaa9926005867e2810adb3e43cd19d49
Kyle Noble <nobleky@oregonstate.edu>
2019-05-21 17:10:37 -0700

    added example PS2 keyboard output and made txt files for easier parsing. Also added python script to parse the txt files.

---
+++ example_input_parse/csvread.py
@@ -0,0 +1,29 @@
+import csv
+
+prev_clk_high = True
+
+values = []
+
+logic_values = []
+
+with open('g.txt') as csv_file:
+    reader = csv.reader(csv_file, delimiter=',')
+    line = 0
+    for row in reader:
+        if line == 0:
+            # Column names
+            line += 1
+        else:
+            if float(row[1]) < 1 and prev_clk_high:
+                values.append(float(row[2]))
+                prev_clk_high = False
+            elif float(row[1]) > 4 and not prev_clk_high:
+                prev_clk_high = True
+            line += 1
+for value in values:
+    if value < 2:
+        logic_values.append(0)
+    else:
+        logic_values.append(1)
+
+print logic_values
